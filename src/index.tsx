2ddf592d2ae74e74077144f062f80131d692f549
8brandon <34548660+8brandon@users.noreply.github.com>
2020-10-31 11:05:20 +0100

    Add react-select component which works when built

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,7 @@
 import * as React from 'react';
-import { Button } from '@chakra-ui/core';
+import { Button } from '@chakra-ui/button';
+import ReactSelect from 'react-select';
 
 export const MyButton = () => <Button colorScheme="green" />;
+
+export const MyCustomSelect = () => <ReactSelect />;
